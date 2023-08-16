9364dae912ca25d146e07d75f4d6b40b13d65b17
Joel Boehland <jboehland@gmail.com>
2023-08-16 11:32:08 -0700

    Add a couple more CLOS utilities. method->function

--- src/core/utilities.lisp
+++ src/core/utilities.lisp
@@ -26,15 +26,36 @@ Example:
     result))
 ;; (%extract-args '(:a :g) '(:a 1 :b 2 :d 4 :g 5))
 
-(defmethod clos-class-initargs (class)
+(defun clos-class-initargs (class)
   (let ((direct-slots (closer-mop:class-direct-slots class)))
     (loop for slot in direct-slots
           append (closer-mop:slot-definition-initargs slot))))
 ;; (clos-class-initargs (find-class 'simple-prompt-node))
 
-(defmethod clos-class-initargs-all (class)
+(defun clos-class-initargs-all (class)
   (let* ((precedence-list (closer-mop:class-precedence-list class))
          (slots (mapcan #'closer-mop:class-direct-slots precedence-list)))
     (loop for slot in slots
           append (closer-mop:slot-definition-initargs slot))))
 ;; (clos-class-initargs-all (find-class 'simple-prompt-node))
+
+(defun clos-class-readers-for-slot (class slot-name)
+  (let ((direct-slots (closer-mop:class-direct-slots class)))
+    (loop for slot in direct-slots
+          when (string= (closer-mop:slot-definition-name slot) slot-name)
+            return (closer-mop:slot-definition-readers slot))))
+
+(defun clos-class-writers-for-slot (class slot-name)
+  (let ((direct-slots (closer-mop:class-direct-slots class)))
+    (loop for slot in direct-slots
+          when (string= (closer-mop:slot-definition-name slot) slot-name)
+            return (closer-mop:slot-definition-writers slot))))
+
+;; (clos-class-readers-for-slot (find-class 'ai-project-tools/core:tree-node) 'children)
+;; (clos-class-writers-for-slot (find-class 'ai-project-tools/core:tree-node) 'children)
+
+;; (closer-mop:specializer-direct-methods (find-class 'ai-project-tools/core:tree-node))
+;; (closer-mop:specializer-direct-methods (find-class 'ai-project-tools/core:execution-tree-node))
+;; (closer-mop:specializer-direct-methods (find-class 'ai-project-tools/core:runnable))
+;; (closer-mop:specializer-direct-methods (find-class 'ai-project-tools/core::timed))
+;; (closer-mop:specializer-direct-methods (find-class 'ai-project-tools/core:realtime-timed-runnable))
