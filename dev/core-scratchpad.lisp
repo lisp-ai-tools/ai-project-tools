c57b9092099b93511e4b9e58bb3739946b072959
Joel Boehland <jboehland@gmail.com>
2023-09-09 05:29:14 -0700

    journal sketches

--- dev/core-scratchpad.lisp
+++ dev/core-scratchpad.lisp
@@ -2,7 +2,6 @@
 
 (defvar *test-app* nil)
 
-
 (defclass test-parent-task-node (execution-tree-node has-name) ())
 (defclass test-child-task-node (execution-node has-name) ())
 
@@ -10,11 +9,17 @@
   (make-instance 'test-parent-task-node
                  :name "parent"
                  :children (list (make-instance 'test-child-task-node
-                                                :name "child-a")
+                                                :name "child-a"
+                                                :input-keys '("KEY-child-a")
+                                                :output-keys '("VAL-child-a"))
                                  (make-instance 'test-child-task-node
-                                                :name "child-b")
+                                                :name "child-b"
+                                                :input-keys '("KEY-child-b")
+                                                :output-keys '("VAL-child-b"))
                                  (make-instance 'test-child-task-node
-                                                :name "child-c"))))
+                                                :name "child-c"
+                                                :input-keys '("KEY-child-c")
+                                                :output-keys '("VAL-child-c")))))
 
 #+(or) (defparameter *test-parent-task-node* (%make-test-parent-task-node))
 #+(or) (defparameter *test-root-store* nil)
@@ -28,13 +33,13 @@
 (defun %run-child-task-nodes (parent-node)
   (flet ((%run-child-task-node (child-node)
            (let* ((store (make-instance 'memory-scoped-metadata-store
-                                       :name (name child-node)
-                                       :parent *test-root-store*
-                                       :schema (list :fake :schema)))
+                                        :name (name child-node)
+                                        :parent *test-root-store*
+                                        :schema (list :fake :schema)))
                   (*current-metadata-store* store))
              (format t "Running ~a, with store: ~a~%" (name child-node) store)
              (run child-node))))
-    (mapcar #'%run-child-task-node (children parent-node))))
+    (map nil #'%run-child-task-node (children parent-node))))
 
 (defmethod run ((node test-parent-task-node) &rest args)
   (setf *test-root-store* nil)
@@ -48,5 +53,18 @@
     (format t "Running ~a, with store: ~a~%" (name node) store)
     (%run-child-task-nodes node)))
 
-#+(or) (let ((parent-node (%make-test-parent-task-node)))
-         (run parent-node))
+#+(or) (progn (journal:jtrace run))
+#+(or) (journal:with-journaling (:record t)
+         (journal:journaled (node-journal)
+           (let ((parent-node (%make-test-parent-task-node)))
+             (run parent-node))
+           (journal:list-events)))
+
+#+(or)(let ((bundle (journal:make-file-bundle "/tmp/user-example/"))
+            (parent-node (%make-test-parent-task-node)))
+        (format t "Recording")
+        (journal:with-bundle (bundle)
+          (run parent-node))
+        (format t "~%Replaying")
+        (journal:with-bundle (bundle)
+          (run parent-node)))
